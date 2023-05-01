06e7d880884f02226d1a4bfd5ba681e7343d8989
WaxifAlee <waxif.alee1@gmail.com>
2023-05-01 16:22:42 +0500

    Navbar Initial

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className="navbar">
+        <p>Hello World</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+
+import { Navbar, About } from "../components";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -12,6 +14,10 @@ export default function Home() {
           content="Home Page Of The Al-Together Foundation."
         />
       </Head>
+
+      <div className={styles.container}>
+        <Navbar />
+      </div>
     </div>
   );
 }
