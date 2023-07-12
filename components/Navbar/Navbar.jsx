ed960b6e0486d96e307711a8aad147259cca90b1
WaxifAlee <waxif.alee1@gmail.com>
2023-07-12 06:42:54 +0500

    Made the navbar responsive with a few animations and active nav links for relative page. A honest night work and it's almost 7 in the morning

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+import {
+  FaInstagram,
+  FaFacebook,
+  FaTwitter,
+  FaTimes,
+  FaBars,
+} from "react-icons/fa";
 
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
   const socialIcons = [
-    { icon: <FaInstagram />, link: "https://instagram.com" },
-    { icon: <FaFacebook />, link: "https://facebook.com" },
-    { icon: <FaTwitter />, link: "https://twitter.com" },
+    { icon: <FaInstagram />, link: "https://instagram.com", text: "Instagram" },
+    { icon: <FaFacebook />, link: "https://facebook.com", text: "Facebook" },
+    { icon: <FaTwitter />, link: "https://twitter.com", text: "Twitter" },
   ];
 
   const navLinks = [
@@ -22,6 +29,10 @@ const Navbar = () => {
     "contact",
     "donate",
   ];
+
+  const [navVisibility, setNavVisibility] = useState(false);
+  const [active, setActive] = useState("");
+  const router = useRouter();
 
   return (
     <div className={styles.container}>
@@ -37,12 +48,29 @@ const Navbar = () => {
             {navLinks.map((link, index) => {
               return (
                 <li key={index} className={styles.navbar__navlinks_link}>
-                  <Link href={link === "home" ? "/" : `/${link}`}>{link}</Link>
+                  <Link
+                    style={
+                      router.pathname === (link === "home" ? "/" : `/${link}`)
+                        ? { fontWeight: "bold" }
+                        : {}
+                    }
+                    href={link === "home" ? "/" : `/${link}`}
+                  >
+                    {link}
+                  </Link>
                   <div className={styles.circle}></div>
                 </li>
               );
             })}
           </ul>
+        </div>
+
+        <div className={styles.icons}>
+          {navVisibility === true ? (
+            <FaTimes onClick={() => setNavVisibility(false)} />
+          ) : (
+            <FaBars onClick={() => setNavVisibility(true)} />
+          )}
         </div>
 
         <div className={styles.navbar__socialMedia_container}>
@@ -55,6 +83,41 @@ const Navbar = () => {
               );
             })}
           </ul>
+        </div>
+      </div>
+
+      <div
+        style={
+          navVisibility === true ? { display: "block" } : { display: "none" }
+        }
+        className={styles.navbar__miniNavBar}
+      >
+        <div className={styles.links_container}>
+          <ul>
+            {navLinks.map((link, index) => {
+              return (
+                <li key={index} className={styles.navbar__miniNavBar_links}>
+                  <Link href={link === "home" ? "/" : `/${link}`}>
+                    {link.toUpperCase()}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <hr color="#5f2c01" />
+          <div className={styles.navbar__socialMedia_container}>
+            <ul>
+              {socialIcons.map((item, index) => {
+                return (
+                  <li key={index} className={styles.navbar__socialMedia_link}>
+                    <Link href={item.link}>
+                      {item.icon} &nbsp; {item.text}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
